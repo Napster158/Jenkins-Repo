018103751b07dcf50f907ace9d5c43ea02ecfa90
Harshit <talktoharshit.er@gmail.com>
2022-05-20 00:21:13 +0530

    new commit

--- jenki.py
+++ jenki.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
 print ("This is the beginning of the code run")
-print ("Current date time is ", str(datetime.now))
+print ("Current date time is ", str(datetime.now()))
 print ("this is the end of the code run")
+print ("--------------------------------------------------")
+
+print ("Watching Panchayat at:", datetime.now())
