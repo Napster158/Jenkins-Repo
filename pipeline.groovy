fed262995360bf9060b7714b7bc198ce328ea907
Napster158 <talktoharshit.er@gmail.com>
2022-06-09 13:11:27 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,14 +2,11 @@ import hudson.FilePath
 import hudson.model.*
 
 def findClonedRepos(manager){
-    def fail_type = ""
-    TEXT= "github"
     def lines = manager.build.logFile.readLines()
     def result = lines.findAll{it.contains("github")}
     manager.listener.logger.println(result.toString());
     manager.listener.logger.println("---------------------");
 
-
-    def fp = new FilePath(manager.build.workspace, 'results')
-    fp.write(results.toString(), null)
+    def fp = new FilePath(manager.build.workspace, 'result')
+    fp.write(result.toString(), null)
 }
