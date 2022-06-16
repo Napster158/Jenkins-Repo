13751b92c2578bf6af9f6521180a4738998c44ed
Harshit <talktoharshit.er@gmail.com>
2022-06-16 11:57:45 +0530

    test regex

--- jenki.py
+++ jenki.py
@@ -7,3 +7,8 @@ print ("--------------------------------------------------")
 
 print ("Watching Panchayat at:", datetime.now())
 print ("test poll scm")
+
+print ("There is a subsequent Error cloning remote repo and needs to be looked at")
+
+print ("I think NNC build (SCaffe) build failed Please confirm.")
+print ("sdhighidhfg jhibasdbf wdighsfhkh sjhigjisdhgfjkphsad Error occurred while compiling NN models NPU compiler")
--- pipeline.groovy
+++ pipeline.groovy
@@ -10,3 +10,37 @@ def findClonedRepos(manager){
     def fp = new FilePath(manager.build.workspace, 'result')
     fp.write(result.toString(), null)
 }
+
+def setFailType(fail_type, type){
+    if (!fail_type.isEmpty()) {
+        if (!fail_type.contains(type)) {
+            fail_type += "," + type
+    }
+    } else {
+        fail_type += type
+    }
+    return fail_type
+}
+
+def failType(manager){
+    def fail_type = ""
+    def previous_fail_type = ""
+    if (manager.logContains(".*Error cloning remote repo.*")) {
+        fail_type = setFailType(fail_type, "Git_Clone")
+    }
+
+    if (manager.logContains(".*NNC build (SCaffe) build failed.*")) {
+        fail_type = setFailType(fail_type, "SCaffe TV failure")
+    }
+
+    if (manager.logContains(".*Error occurred while compiling NN models NPU compiler.*")) {
+        fail_type = setFailType(fail_type, "Git_Clone")
+    }
+
+    def fp = new FilePath(manager.build.workspace, 'fail_type')
+    if (fp.exists()) {
+        previous_fail_type = fp.readToString()
+    }
+    fail_type=setFailType(fail_type, previous_fail_type)
+    fp.write(fail_type, null)
+}
